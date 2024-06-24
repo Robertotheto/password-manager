bc362ce5d4ccd024058985dffd74ccee913b1487
Roberto Dos Santos Filho <robertodossantosfilho1001@gmail.com>
2024-06-24 17:45:01 +0000

    feat: add cors and jwt secret and env file example.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,18 @@
 import {fastify} from 'fastify'
+import fastifyCors from '@fastify/cors'
+import fastifyJwt from '@fastify/jwt'
+import 'dotenv/config'
 
-export const app = fastify()
+export const app = fastify({logger: true})
+
+app.register(fastifyCors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+})
+
+app.register(fastifyJwt, {
+    secret: process.env.JWT_SECRET as string
+})
 
 app.get('/', async (request, reply) => {
     return {hello: 'world'}
